c23bb2923a6501826cc1df12935ae9e2cb1a17c1
pepe-roni <ahde2k@gmail.com>
2017-06-14 00:53:02 -0700

    sleep functionality

--- secondBatch/ring.cpp
+++ secondBatch/ring.cpp
@@ -1,5 +1,6 @@
 #include "Particle.h"
 
+unsigned long firstAvailable = 0;
 int buttonState;
 
 void setup()
@@ -11,5 +12,24 @@ void setup()
 void loop()
 {
     buttonState = digitalRead(D1);
+    bool wifiReady = WiFi.ready();
+	bool cloudReady = Particle.connected();
+	if (wifiReady) {
+		if (firstAvailable == 0) {
+			firstAvailable = millis();
+		}
+		if (millis() - firstAvailable > 30000) {
+			// After we've been up for 30 seconds, go to sleep
+			Serial.println("calling System.sleep(30)");
+			System.sleep(30);
 
+			// This happens immediately - System.sleep(30) doesn't block and your code keeps running, just with
+			// Wi-Fi and cloud turned off. The LED should breathe white instead of cyan in this state. Then,
+			// after the time expires, reconnection to the cloud occurs
+			Serial.println("returned from sleep");
+		}
+	}
+	else {
+		firstAvailable = 0;
+	}
 }
