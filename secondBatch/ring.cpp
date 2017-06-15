37df0f7932d348b8bcec5021c765ffe907093e78
Derrick Ng <ahde2k@gmail.com>
2017-06-14 18:16:06 -0700

    replaced sleep functionality with wifi module shutdown to reduce power draw
    also function to check wifi on/off, switch led when published.

--- secondBatch/ring.cpp
+++ secondBatch/ring.cpp
@@ -1,35 +1,42 @@
 #include "Particle.h"
 
-unsigned long firstAvailable = 0;
 int buttonState;
 
 void setup()
 {
     Particle.variable("bs", buttonState);
     pinMode(D1,INPUT_PULLUP);
+    pinMode(D2, OUTPUT);
 }
 
-void loop()
+void loop() 
 {
     buttonState = digitalRead(D1);
-    bool wifiReady = WiFi.ready();
-	bool cloudReady = Particle.connected();
-	if (wifiReady) {
-		if (firstAvailable == 0) {
-			firstAvailable = millis();
-		}
-		if (millis() - firstAvailable > 30000) {
-			// After we've been up for 30 seconds, go to sleep
-			Serial.println("calling System.sleep(30)");
-			System.sleep(30);
+    
+    if(buttonState == 1){
+        WiFi.off();
+         wifiLEDHandler();
+    }
+    else
+    {
+        WiFi.on();
+        delay(7500);
+        wifiLEDHandler();
+        Particle.publish("bell rung");
+        delay(7500);
+    }
+}
 
-			// This happens immediately - System.sleep(30) doesn't block and your code keeps running, just with
-			// Wi-Fi and cloud turned off. The LED should breathe white instead of cyan in this state. Then,
-			// after the time expires, reconnection to the cloud occurs
-			Serial.println("returned from sleep");
-		}
-	}
-	else {
-		firstAvailable = 0;
-	}
+bool wifiLEDHandler()
+{
+    bool wifiReady = WiFi.ready();
+    if(wifiReady){
+         digitalWrite(D2, HIGH);
+         return true;
+    }
+    else
+    {
+        digitalWrite(D2,LOW);
+        return false;
+    }
 }
